5bde3b5857942b6ad98dfb14eaaf313394ec89fe
KyuCheol Hwang <97iambigman@gmail.com>
2023-04-18 16:08:17 +0900

    fix(/util/RandomNumberGenerator) : 테스트 통과를 위해 숫자를 랜덤하게 하나씩 고르는 로직으로 변경

--- src/main/java/baseball/util/RandomNumberGenerator.java
+++ src/main/java/baseball/util/RandomNumberGenerator.java
@@ -9,26 +9,18 @@ public class RandomNumberGenerator {
      * @return 3자리 숫자이면서, 각 자리 숫자는 모두 다르고 1~9 범위 안의 숫자를 반환
      */
     public static int generate() {
-        while (true) {
-            int randomNumber = Randoms.pickNumberInRange(100, 999);
-            if (isValidate(randomNumber)) {
-                return randomNumber;
-            }
-        }
-    }
-
-    /** 비즈니스 로직 **/
-    // 조건 : 각 자리 숫자 모두 1~9이면서, 다른 숫자
-    private static boolean isValidate(int number) {
+        int result = 0;
         boolean[] used = new boolean[10];
-        while (number != 0) {
-            int remainder = number % 10;
-            if (remainder == 0 || used[remainder]) {
-                return false;
+        int count = 3;
+        while (count > 0) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (used[randomNumber]) {
+                continue;
             }
-            used[remainder] = true;
-            number /= 10;
+            used[randomNumber] = true;
+            result = result * 10 + randomNumber;
+            count--;
         }
-        return true;
+        return result;
     }
 }
--- src/test/java/baseball/util/RandomNumberGeneratorTest.java
+++ src/test/java/baseball/util/RandomNumberGeneratorTest.java
@@ -11,7 +11,7 @@ class RandomNumberGeneratorTest {
 
     @Test
     @DisplayName("생성하는 숫자는 3자리 숫자이면서, 각 자리 숫자는 모두 다르고 1~9 범위 안 입니다.")
-    void test() {
+    void generateTest() {
         int count = 0;
         while (count < 100) {
             int randomNumber = RandomNumberGenerator.generate();
@@ -19,6 +19,14 @@ class RandomNumberGeneratorTest {
             assertThat(isValidate(randomNumber)).isTrue();
             count++;
         }
+    }
+
+    @Test
+    @DisplayName("0이 있거나 중복되는 숫자가 있을 시 false를 반환합니다.")
+    void isValidateTest() {
+        assertThat(isValidate(102)).isFalse();
+        assertThat(isValidate(313)).isFalse();
+        assertThat(isValidate(123)).isTrue();
     }
 
     private boolean isValidate(int number) {
